983804c05e35f2e5fc698cb59521e64b01642eba
pparth602 <pparth602@yahoo.com>
2022-12-31 11:50:50 +0530

    added circuitbreaker for ORDER SERVICE

--- OrderService/src/main/java/com/pspatel/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/pspatel/OrderService/external/client/PaymentService.java
@@ -1,15 +1,22 @@
 package com.pspatel.OrderService.external.client;
 
+import com.pspatel.OrderService.exception.CustomException;
 import com.pspatel.OrderService.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient("PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
   @PostMapping
   public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+  default void fallback(Exception e){
+    throw new CustomException("Payments Service is not available", "UNAVAILABLE",500);
+  }
 
 }
--- OrderService/src/main/java/com/pspatel/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/pspatel/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.pspatel.OrderService.external.client;
 
+import com.pspatel.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -13,5 +16,9 @@ public interface ProductService {
   ResponseEntity<Void> reduceQuantity(
       @PathVariable("id") Long productId,
       @RequestParam Long quantity);
+
+  default void fallback(Exception e){
+    throw new CustomException("Product Service is not available", "UNAVAILABLE",500);
+  }
 
 }

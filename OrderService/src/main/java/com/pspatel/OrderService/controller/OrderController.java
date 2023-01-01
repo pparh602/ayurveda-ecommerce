2c5e7b71b047db7f0103e22d53a3d6a1116f5a7c
pparth602 <pparth602@yahoo.com>
2023-01-01 19:20:56 +0530

    added okta auth and spring security to services

--- OrderService/src/main/java/com/pspatel/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/pspatel/OrderService/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ public class OrderController {
 
   @Autowired private OrderService orderService;
 
+  @PreAuthorize("hasAuthority('Customer')")
   @PostMapping("/placeOrder")
   public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest){
     Long orderId = orderService.placeOrder(orderRequest);
@@ -28,6 +30,7 @@ public class OrderController {
     return new ResponseEntity<>(orderId, HttpStatus.OK);
   }
 
+  @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer')")
   @GetMapping("/{orderId}")
   public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId){
     OrderResponse orderResponse = orderService.getOrderDetails(orderId);
--- ProductService/src/main/java/com/pspatel/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/pspatel/ProductService/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.pspatel.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +23,15 @@ public class ProductController {
   @Autowired
   ProductService productService;
 
+  @PreAuthorize("hasAuthority('Admin')")
   @PostMapping
   public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
     Long productId = productService.addProduct(productRequest);
     return new ResponseEntity<>(productId, HttpStatus.CREATED);
   }
 
+  @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer') || hasAuthority"
+      + "('SCOPE_internal')")
   @GetMapping("/{id}")
   public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") Long productId) {
     ProductResponse productResponse
